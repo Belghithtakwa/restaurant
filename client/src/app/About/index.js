b0a337446b875c7d10f045ef7721b7934c871c68
takwa <blgeyatatou@gmail.com>
2022-05-08 15:39:02 +0200

    fix front

--- client/src/app/About/index.js
+++ client/src/app/About/index.js
@@ -4,10 +4,24 @@ const About = () => {
   return (
     <Fragment>
     <div class="lg:2/6 xl:w-2/4 mt-20 lg:mt-40 lg:ml-16 text-left">
-            <div className="text-6xl font-semibold text-gray-900 leading-none">Bring all your work together</div>
-            <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">A better experience for yout attendees and less stress yout team.</div>
-           <img src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" 
-           className=" lg:h-2/6 w-3/5"/>
+            <div className="text-6xl font-semibold text-primary leading-none">A major ally for the digitalization of your restaurant.</div>
+            <div className="mt-6 text-xl font-bold text-primary-shade antialiased">Easy to use and quick to set up, it takes no commission.​​​​​
+           
+            </div>
+            <div>
+            <ul className='p-8 list-disc font-semibold'>
+              <li>
+              sell its products in click and collect 
+              </li>
+              <li>allow your customers to order and pay from their table</li>
+            </ul>
+            </div>
+            <div className=' bg-gray-900 shadow-2xl'>
+            <img src="https://www.deliver-by-linkeo.com/ressources/images/69f19e1405ca.jpg" 
+           className=" border-black rounded-xl content-end absolute inset-y-1/4 shadow-2xl
+            right-12"/>
+            </div>
+          
           </div>
       <div>
         <img />
